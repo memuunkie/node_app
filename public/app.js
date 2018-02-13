08a6bdceb5c120b0eb9f5b538be5d831fe491b19
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-02-13 12:02:45 -0800

    Added AJAX calls to display todos on page.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
-alert("HI FROM APP.JS");
+$(document).ready(function() {
+	$.getJSON('/api/todos')
+	.then(addTodos)
+	.catch(function(err) {
+		res.send(err);
+	})
+});
+
+function addTodos(todos) {
+	// adds todos to page
+	todos.forEach(function(todo) {
+		var newTodo = $('<li>' + todo.name + '</li>');
+		$('.list').append(newTodo);
+	})
+}
