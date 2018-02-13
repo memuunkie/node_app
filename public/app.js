31431320727dd0c931e18eda6516ee97ad448359
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-02-13 13:33:25 -0800

    Added DELETE function to remove todos from database

--- public/app.js
+++ public/app.js
@@ -1,26 +1,34 @@
 $(document).ready(function() {
+	// display current list
 	$.getJSON('/api/todos')
 	.then(addTodos)
 	.catch(function(err) {
 		console.log(err);
-	})
-
+	});
+	// listening for 'Enter' after entering in new task
 	$('#todoInput').keypress(function(event) {
 		if(event.which == 13) {
 			createTodo();
 		}
-	})
+	});
+	// listener for 'X' span click
+	$('.list').on('click', 'span', function() {
+		removeTodo($(this).parent());
+	});
+
 });
 
 function addTodos(todos) {
-	// adds todos to page
+	// adds todos from database to page
 	todos.forEach(function(todo) {
 		addTodo(todo);
 	});
 }
 
 function addTodo(todo) {
-	var newTodo = $('<li class="task">' + todo.name + '</li>');
+	// add individual todo and conditional formatting
+	var newTodo = $('<li class="task">' + todo.name + ' <span>X</span></li>');
+	newTodo.data('id', todo._id);
 	if(todo.completed) {
 		newTodo.addClass('done');
 	}
@@ -38,4 +46,20 @@ function createTodo() {
 	.catch(function(err) {
 		console.log(err);
 	})
+}
+
+function removeTodo(todo) {
+	// delete todo from database
+	var clickedId = todo.data('id');
+	var deleteUrl = '/api/todos/' + clickedId;
+	$.ajax({
+		method: 'DELETE',
+		url: deleteUrl
+	})
+	.then(function(data) {
+		todo.remove();
+	})
+	.catch(function(err) {
+		console.log(err);
+	});
 }
